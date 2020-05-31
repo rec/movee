29940e2f1a09f1eb3cdac50a5036d535143684e7
Tom Ritchford <tom@swirly.com>
2020-05-31 12:15:13 +0200

    Import some code from gitz

---
+++ ascript/cast.py
@@ -0,0 +1,75 @@
+"""
+Represents a single asciinema file for reading or writing
+"""
+
+from . import constants
+from pathlib import Path
+import json
+import safer
+
+EXIT = 'exit' + constants.RETURN
+EPSILON = 0.001
+
+
+class Cast:
+    def __init__(self, lines=None, header=None):
+        self.lines = lines or []
+        self.header = constants.HEADER if header is None else header
+
+    @classmethod
+    def read(cls, fp):
+        if isinstance(fp, (str, Path)):
+            with open(fp) as fp2:
+                return cls.read(fp2)
+
+        lines = []
+
+        for i, line in enumerate(fp):
+            value = json.loads(line)
+            if i:
+                assert isinstance(value, list)
+                lines.append(value)
+            else:
+                assert isinstance(value, dict)
+                header = value
+        return cls(lines, header)
+
+    def append(self, keys, delta_time):
+        dt = 0 if delta_time < EPSILON else delta_time
+        self.lines.append([dt, 'o', keys])
+
+    def write(self, fp):
+        if isinstance(fp, (str, Path)):
+            with safer.writer(fp) as fp2:
+                return self.write(fp2)
+
+        for i in (self.header, *self.lines):
+            print(json.dumps(i), file=fp)
+
+    def scale(self, ratio):
+        for line in self.lines:
+            line[0] *= ratio
+
+    def replace_prompt(self):
+        # Unused
+        original = self.lines[0][2]
+        for line in self.lines:
+            line[2] = line[2].replace(original, constants.PROMPT, 1)
+
+    def remove_exit(self):
+        # Unused
+        last = self.lines[-1]
+        if last[2] == EXIT:
+            last[2] = last[2][4:]
+
+    def extend(self, other, offset=0):
+        for c in 'width', 'height':
+            s = self.header.get(c, 0)
+            o = other.header.get(c, 0)
+            m = max(s, o)
+            if m > 0:
+                self.header[c] = m
+
+        if self.lines:
+            offset += self.lines[-1][0]
+        self.lines.extend([t + offset, i, k] for t, i, k in other.lines)
---
+++ ascript/constants.py
@@ -0,0 +1,46 @@
+from . import colors
+from pathlib import Path
+
+PROMPT = '▶ {BLUE}tom{RED}:{GREEN}/code/test{NONE}$ '
+PROMPT = PROMPT.format(**vars(colors))
+HEADER = {'version': 2, 'width': 100, 'height': 40}
+
+BACKSPACE = '\x08\x1b[K'
+RETURN = '\r\n'
+CONTROL_L = '\x1b[H\x1b[2J'
+
+OLD = False
+
+if OLD:
+    FILE = Path(__file__)
+    ROOT = FILE.parents[2]
+
+    DOC_DIR = ROOT / 'doc'
+
+    MOVIES_ROOT = ROOT / 'gitz_doc/movies'
+    CAST_DIR = MOVIES_ROOT / 'recorded'
+
+    FILES = {
+        'svg': DOC_DIR / 'movies',
+        'cast': DOC_DIR / 'cast',
+        'sh': MOVIES_ROOT / 'scripted',
+    }
+
+    ALL_COMMANDS = 'all-gitz'
+
+    MAX_KEYSTROKE_TIME = 0.7
+    KEYSTROKE_TIME_SCALE = 0.32
+    TIME_TO_THINK = 1
+    TIME_AT_END = 5
+    TIME_TO_READ_ONE_CHAR = 0.005
+    TYPING_ERROR_RATE = 0.06
+
+    def command_file(command, name):
+        dirname = FILES[name]
+        return (dirname / command).with_suffix('.' + name)
+
+    def recorded_cast_files():
+        yield from (f for f in CAST_DIR.iterdir() if f.suffix == '.cast')
+
+    ALL_COMMANDS_CAST = command_file(ALL_COMMANDS, 'cast')
+    ALL_COMMANDS_JSON = ALL_COMMANDS_CAST.with_suffix('.json')
