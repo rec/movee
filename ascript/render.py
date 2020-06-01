03fd770c4722076b672c65e7b5b091826aa10162
Tom Ritchford <tom@swirly.com>
2020-06-01 19:06:42 +0200

    Refactor unused render.py

--- ascript/render.py
+++ ascript/render.py
@@ -8,16 +8,16 @@ import tempfile
 TEMPLATE = 'solarized_light'
 
 
-def render(cast, svg_file):
+def render(cast, svg_file, template=TEMPLATE):
     with tempfile.TemporaryDirectory() as td:
         cast_file = Path(td) / 'file.cast'
         cast.write(cast_file)
-        _render_file(cast_file, svg_file)
+        _render_file(cast_file, svg_file, template)
 
 
-def _render_file(cast_file, svg_file):
+def _render_file(cast_file, svg_file, template):
     asciicast_records = asciicast.read_records(str(cast_file))
     geometry, frames = term.timed_frames(asciicast_records)
 
-    template = config.default_templates()[TEMPLATE]
-    anim.render_animation(frames, geometry, str(svg_file), template)
+    tmpl = config.default_templates()[template]
+    anim.render_animation(frames, geometry, str(svg_file), tmpl)
