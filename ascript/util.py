27046de7dc2539c99fd4da8873e1cf3c3f44dba1
Tom Ritchford <tom@swirly.com>
2020-06-02 15:31:32 +0200

    New util.trailing_slash()

---
+++ ascript/util.py
@@ -0,0 +1,44 @@
+import hashlib
+
+
+def stable_hash(s):
+    return hashlib.blake2s(s.encode()).hexdigest()
+
+
+def split_comments(lines):
+    line_queue = []
+    was_comment = False
+
+    for line in lines:
+        if line.strip():
+            is_comment = line.strip().startswith('#')
+        else:
+            is_comment = was_comment
+
+        if is_comment != was_comment:
+            if line_queue:
+                yield line_queue
+                line_queue = []
+
+        line_queue.append(line)
+        was_comment = is_comment
+
+    if line_queue:
+        yield line_queue
+
+
+def trailing_slash(lines):
+    line_queue = []
+
+    for line in lines:
+        if line.strip().endswith('\\'):
+            line_queue.append(line[:-1])
+        elif line_queue:
+            line_queue.append(line)
+            yield ''.join(line_queue)
+            line_queue.clear()
+        else:
+            yield line
+
+    if line_queue:
+        yield ''.join(line_queue)
---
+++ test/test_util.py
@@ -0,0 +1,58 @@
+from ascript import util
+from unittest import TestCase
+
+
+def split(lines):
+    return list(util.split_comments(lines))
+
+
+def trailing(lines):
+    return list(util.trailing_slash(lines))
+
+
+class SplitCommentsTest(TestCase):
+    def test_empty(self):
+        assert split([]) == []
+
+    def test_single_split(self):
+        assert split('a') == [['a']]
+        assert split('#') == [['#']]
+
+    def test_simple(self):
+        lines = 'a#a'
+        actual = split(lines)
+        expected = [[i] for i in lines]
+        assert actual == expected
+
+    def test_complex(self):
+        lines = ' ## # X XX#X'
+        actual = split(lines)
+        expected = [
+            [' '],
+            ['#', '#', ' ', '#', ' '],
+            ['X', ' ', 'X', 'X'],
+            ['#'],
+            ['X'],
+        ]
+        assert actual == expected
+
+
+class TrailingSlashTest(TestCase):
+    def test_empty(self):
+        assert trailing([]) == []
+
+    def test_no_trailing(self):
+        assert trailing('a') == ['a']
+        assert trailing('abc') == ['a', 'b', 'c']
+
+    def test_simple(self):
+        lines = ['a', 'b\\', 'c\\', 'd', 'e']
+        actual = trailing(lines)
+        expected = ['a', 'bcd', 'e']
+        assert actual == expected
+
+    def test_edge(self):
+        lines = ['a', 'b\\', 'c\\']
+        actual = trailing(lines)
+        expected = ['a', 'bc']
+        assert actual == expected
