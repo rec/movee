cf61de1c99b9eeb0bc24327df2b50b1c3880a65b
Tom Ritchford <tom@swirly.com>
2020-06-02 13:54:20 +0200

    Remove obsolete constants

--- ascript/constants.py
+++ ascript/constants.py
@@ -9,13 +9,5 @@ RETURN = '\r\n'
 CONTROL_L = '\x1b[H\x1b[2J'
 
 
-MAX_KEYSTROKE_TIME = 0.7
-KEYSTROKE_TIME_SCALE = 0.32
-TIME_TO_THINK = 1
-TIME_AT_END = 5
-TIME_TO_READ_ONE_CHAR = 0.005
-TYPING_ERROR_RATE = 0.06
-
-
 def stable_hash(s):
     return hashlib.blake2s(s.encode()).hexdigest()
