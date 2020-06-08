42889a11c356352aa6b9f822259cb21029eb4a28
Tom Ritchford <tom@swirly.com>
2020-06-08 20:10:44 +0200

    Get rid of experimental code

--- ascript/runner.py
+++ ascript/runner.py
@@ -1,110 +1,83 @@
-from queue import Queue
-from subprocess import Popen, PIPE
-from threading import Thread
-from .util import print_exception
-import contextlib
-import sys
+from asyncio.subprocess import PIPE
+import asyncio
+import itertools
+import shlex
+import uuid
 
-BASH = '/bin/bash', '-i'
-MARKER = 'xyzzyxyzzy'
-STDOUT, STDERR, STDIN = range(3)
-NAMES = 'stdout', 'stderr', 'stdin'
-POPEN_ARGS = {
-    'stdin': PIPE,
-    'stdout': PIPE,
-    'stderr': PIPE,
-    'encoding': 'utf8',
-    'shell': not True,
-}
+MARKER = str(uuid.uuid4()) + '.'
+BASH_PROMPT = f'PS1="{MARKER}1\\n" ; PS2="{MARKER}2\\n"'
+PYTHON_PROMPT = ';'.join(
+    ['import sys', f'sys.ps1 = "{MARKER}1\\n"', f'sys.ps2 = "{MARKER}2\\n"']
+)
 
-# https://dzone.com/articles/interacting-with-a-long-running-child-process-in-p
+ERR, OUT, IN, PROMPT = 'EOIP'
 
 
-@contextlib.contextmanager
-def run(cmd, marker, callback, timeout=0.2):
-    proc = Popen(cmd, **POPEN_ARGS)
+async def run(execute, set_prompts, commands, callback, shell=False):
+    cmd = execute
+    if shell:
+        create = asyncio.create_subprocess_shell
+        if not isinstance(cmd, str):
+            cmd = [shlex.join(cmd)]
+    else:
+        create = asyncio.create_subprocess_exec
+        if isinstance(cmd, str):
+            cmd = shlex.split(cmd)
 
-    def printline(*parts):
-        print(*parts, file=proc.stdin)
-        proc.stdin.flush()
+    ready = asyncio.Event()
+    ready.set()
 
-    proc.printline = printline
+    proc = await create(*cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-    def read(stream):
-        name = STDERR if stream is proc.stderr else STDOUT
-        line = True
-        while line or proc.poll() is None:
-            line = stream.readline()
-            line and callback(name, line.rstrip('\n'))
+    async def read_out():
+        while line := await proc.stdout.readline():
+            callback(OUT, line.decode('utf8').rstrip('\n'))
 
-        callback(name, None)
+    async def read_err():
+        first_prompt = True
 
-    for stream in proc.stderr, proc.stdout:
-        Thread(target=read, args=(stream,), daemon=True).start()
+        while line := await proc.stderr.readline():
+            before, *after = line.decode('utf8').split(MARKER, maxsplit=1)
 
-    try:
-        yield proc
+            if after:
+                if first_prompt:
+                    first_prompt = False
+                elif before:
+                    callback(ERR, before)
+                callback(PROMPT, after[0].strip())
+                ready.set()
+            elif not first_prompt:
+                callback(ERR, before.rstrip('\n'))
 
-    finally:
-        with print_exception():
-            proc.stdin.close()
+    async def write():
+        for command in itertools.chain((set_prompts,), commands):
+            await ready.wait()
+            ready.clear()
 
-        with print_exception():
-            proc.terminate()
-            proc.wait(timeout=timeout)
+            if command is not set_prompts:
+                callback(IN, command)
+            line = command.encode('utf8') + b'\n'
+            proc.stdin.write(line)
+            await proc.stdin.drain()
+        proc.kill()
 
-
-def shell_commands(commands, callback, shell=BASH, marker='echo {}'):
-    marker = marker.format(MARKER)
-    finished = 0
-    queue = Queue()
-
-    with run(shell, marker, lambda *a: queue.put(a)) as proc:
-        for i, command in enumerate(commands):
-            callback(STDIN, command)
-            proc.printline(command)
-            proc.printline(marker)
-
-            line = ''
-            while finished < 2:
-                name, line = queue.get()
-                if line and marker in line.strip():
-                    break
-
-                callback(name, line)
-                finished += line is None
-            else:
-                raise ValueError('End without marker')
-
-        callback(STDIN, None)
+    await asyncio.gather(read_err(), read_out(), write())
 
 
-def printer(name, line):
-    print('%7s:' % NAMES[name], line)
+def yield_inputs(prompt):
+    while True:
+        s = input(prompt)
+        if not s:
+            break
+        yield s
 
 
-def print_shell_commands(filename, shell=BASH, marker='echo {}'):
-    shell_commands(
-        open(filename), callback=printer, shell=shell, marker=marker
-    )
+def main():
+    commands = 'echo HELLO', 'ls', 'echo BYE'
+    # commands = yield_inputs('$ ')
 
-
-def print_python_commands(filename):
-    print_shell_commands(filename, ('python3', '-i'), "print('{}')")
-
-
-def run_python_commands():
-    def commands():
-        while True:
-            s = input('>>> ')
-            if not s:
-                break
-            yield s
-
-    shell_commands(commands(), printer, ('python3', '-i'), "print('{}')")
+    asyncio.run(run('/bin/bash -i', BASH_PROMPT, commands, print))
 
 
 if __name__ == '__main__':
-    # print_shell_commands(sys.argv[1])
-    print_python_commands(sys.argv[1])
-    # run_python_commands()
+    main()
