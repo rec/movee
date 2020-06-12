6df72df4613ed922f12cb3239ddba02285c3bb6c
Tom Ritchford <tom@swirly.com>
2020-06-12 21:00:01 +0200

    Improve test coverage

--- test/test_cast.py
+++ test/test_cast.py
@@ -1,6 +1,9 @@
 from ascript.cast import Cast, EPSILON
 from unittest import TestCase
 import io
+from pathlib import Path
+
+TEST_CAST_FILE = Path(__file__).parents[1] / 'data/recorded/git-copy.cast'
 
 
 def _round_trip(cast):
@@ -37,8 +40,13 @@ class TestCast(TestCase):
         assert cast.lines[0].time == 0
 
     def test_extend(self):
+        cast1 = Cast()
+        cast1.append('ls -cail', 1)
         cast = Cast()
-        cast.append('ls -cail', 1)
+        cast.extend(cast)
+        assert not cast.lines
+
+        cast.extend(cast1)
         cast.extend(cast, 2)
         cast.extend(cast, 4)
         _round_trip(cast)
@@ -49,3 +57,27 @@ class TestCast(TestCase):
             [12, 'o', 'ls -cail'],
         ]
         assert [i.to_list() for i in cast.lines] == expected
+
+    def test_scale(self):
+        cast = Cast()
+        cast.append('ls -cail', 1)
+        cast.extend(cast, 2)
+        cast.extend(cast, 4)
+        cast.scale_by(2)
+        expected = [
+            [2, 'o', 'ls -cail'],
+            [8, 'o', 'ls -cail'],
+            [18, 'o', 'ls -cail'],
+            [24, 'o', 'ls -cail'],
+        ]
+        assert [i.to_list() for i in cast.lines] == expected
+
+    def test_keystroke_times(self):
+        cast = Cast.read(open(TEST_CAST_FILE))
+        actual = list(cast.keystroke_times())
+        for t in actual:
+            print('%s,' % round(t, 3))
+        expected = []
+        assert len(actual) == 48
+        expected = [0.279, 0.177, 0.104, 0.248, 0.103, 0.08, 0.186, 0.224]
+        assert [round(i, 3) for i in actual[:8]] == expected
