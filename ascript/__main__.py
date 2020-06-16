6885c4db80235d26d9b52259c0a8bd2442355253
Tom Ritchford <tom@swirly.com>
2020-06-16 18:50:49 +0200

    Hide or remove cruft

--- ascript/__main__.py
+++ ascript/__main__.py
@@ -1,10 +1,41 @@
-from . import ascript
+"""
+🎦 Script asciinema movies 🎦
+====================================================================
+
+
+USAGE
+-------
+
+.. code-block:: bash
+
+    ascript my_file.py [README.rst]
+
+"""
+
 import sys
 
+__all__ = ('main',)
 
-def main():
-    ascript.ascript(*sys.argv[1:])
+
+def main(source, target=None):
+    """Print documentation for a file or module
+
+    ARGUMENTS
+      path
+        path to the Python file or module.
+
+      target
+        path to the output file or ``None``, in which case
+        output is printed to stdout
+    """
+    lines = []
+
+    if target:
+        with open(target, 'w') as fp:
+            print(lines, file=fp)
+    else:
+        print(lines)
 
 
 if __name__ == '__main__':
-    main()
+    main(*sys.argv[1:])
--- test/test_ascript.py
+++
@@ -1,7 +0,0 @@
-from ascript import ascript
-from unittest import TestCase
-
-
-class TestAscript(TestCase):
-    def test_trivial(self):
-        assert ascript
